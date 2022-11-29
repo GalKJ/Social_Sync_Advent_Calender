6f12284df206516b3b9890bf10fe9f4463817512
space doge <99536044+d0g3bl2@users.noreply.github.com>
2022-11-29 09:24:22 +0000

    add populate local storage function logic

--- src/local_storage.js
+++ src/local_storage.js
@@ -1,17 +1,14 @@
 // Function to create object in local storage if it doesn't exist and render it in the UI.
 function populateStorage(element) {
 
-    localStorage.setItem('Won', '0')
-    localStorage.setItem('Lost', '0')
+    localStorage.setItem('date', '1669852800000')
 
-    let wonTally = localStorage.getItem('Won');
-    let lostTally = localStorage.getItem('Lost');
-
-    element.textContent = `Won:${wonTally} Lost:${lostTally}`;
+    dateSelect.valueAsNumber = localStorage.getItem('date');
+// Do i want to create a local object to hold the date or do i not need to?
 }
 
 // Function to get values from local storage and render them in the UI.
-function updateWinLoseCounterDisplay(element) {
+function retrieveDateValueFromLocalStorage() {
 
     let wonTally = localStorage.getItem('Won');
     let lostTally = localStorage.getItem('Lost');
