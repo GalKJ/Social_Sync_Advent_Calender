494333f6a8dec331155c0d3d21b6830137c9c013
space doge <99536044+d0g3bl2@users.noreply.github.com>
2022-11-29 09:12:29 +0000

    add src folder and local storage file

---
+++ src/local_storage.js
@@ -0,0 +1,20 @@
+// Function to create object in local storage if it doesn't exist and render it in the UI.
+function populateStorage(element) {
+
+    localStorage.setItem('Won', '0')
+    localStorage.setItem('Lost', '0')
+
+    let wonTally = localStorage.getItem('Won');
+    let lostTally = localStorage.getItem('Lost');
+
+    element.textContent = `Won:${wonTally} Lost:${lostTally}`;
+}
+
+// Function to get values from local storage and render them in the UI.
+function updateWinLoseCounterDisplay(element) {
+
+    let wonTally = localStorage.getItem('Won');
+    let lostTally = localStorage.getItem('Lost');
+    
+    element.textContent = `Won:${wonTally} Lost:${lostTally}`;
+}
